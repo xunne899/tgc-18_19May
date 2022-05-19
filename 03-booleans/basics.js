727540823a21e4ba9e95568fa5069b212df65e81
xunne89 <yixun89@live.com>
2022-05-19 11:55:46 +0800

    add some files

---
+++ 03-booleans/basics.js
@@ -0,0 +1,14 @@
+console.log(true)
+console.log(false)
+// comparison operators-comparision will go first 
+console.log("45>=50", 45>50);
+console.log("50 == 50", 50 == 50);
+console.log("50" == 50)
+console.log ('2' + 1)
+console.log("2"*3)
+console.log(50 === "50")
+console.log(50 === parseInt("50"))
+// logical operators
+
+let isLockdown = false ;
+let isVacinnated = true;
