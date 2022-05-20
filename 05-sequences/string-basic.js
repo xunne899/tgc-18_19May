9c5301284fadccc0a889c459becb8ac29bcba5e2
xunne899 <tan.yixun@trentglobal.edu.sg>
2022-05-20 06:23:40 +0000

    add 05

---
+++ 05-sequences/string-basic.js
@@ -0,0 +1,8 @@
+ let s = 'the quick brown fox'
+ let n = "she sells seashells"
+
+ console.log('she said, "i sold seashells"')
+
+ console.log(" i don't know ")
+
+ console.log("Dear mum, \n I hope this letter finds you well")
